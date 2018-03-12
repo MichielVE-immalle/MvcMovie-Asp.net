9d3fdfe8af1c537a6316f803710a7e7b7adfb6d7
Michiel Van Eester <michiel.vaneester@immalle.be>
2018-03-12 14:07:41 +0100

    view, controller, model added

---
+++ MvcMovie Asp.net/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovieAspnet.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieAspnet.Models.Movie> Movie { get; set; }
+    }
+}
